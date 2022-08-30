fdc3f9776379711c57d273d9bb2b3aaa4d5b5d28
Leobin93 <leoayala93@gmail.com>
2022-08-29 23:56:39 -0300

    se agrega proyecto

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
+let cafe = [
 
-let latte = 0;
-let flatwhite = 0;
-let capuccino = 0;
-let moka = 0;
-
-function cafe (leche, cafe, espuma, chocolate, cafeDoble){
-    latte = "leche" + " " + "cafe";
-    flatwhite = "leche" + " " + "cafeDoble";
-    capu = "leche" + " " + "cafe" + " " + "espuma";
-    moka = "leche" + " " + "cafe" + " " + "chocolate";
-}
-
-function mostrar (mensaje){
-    console.log(mensaje);
-}
-
-cafe();
-mostrar();
-
-let hacerCafe = prompt("ingresar bebida");
-let = "leche";
-let = "cafe";
-let = "cafeDoble";
-let = "espuma";
-let = "chocolate";
-
-switch(hacerCafe){
-    case "latte":
-        alert("leche" + "cafe");
-        break;
+    {
     
-    case "flatwhite":
-        alert("leche" + "cafeDoble");
-        break;
-
-    case "capu":
-        alert("leche" + "cafe" + "espuma");
-        break;
-
-    case "moka":
-        alert("leche" + "cafe" + "chocolate");
-        break;
-
-    default: alert("sin cafe");
-}
+    nombre: "latte",
+    
+    descripcion: "50% cafe y 50% leche",
+    
+    precio: 350,
+    
+    id: 01
+    
+    },
+    
+    {
+    
+    nombre: "moka",
+    
+    descripcion: "cafe con chocolate y un poco de leche",
+    
+    precio: 450,
+    
+    id: 02
+    
+    }
+    
+    ]
+    
+    let pregunta = Number(prompt("queres un moka elegi 1 y si queres latte elegi 2"))
+    
+    function cafeElegido(indice){
+    
+    alert("tu cafe contiene: " + cafe[`${indice}`].descripcion);
+    
+    }
+    
+    for (i = 0; i < cafe.length; i++){
+    
+    console.log("el cafe " + cafe[i].nombre + " contiene " + cafe[i].descripcion + "y sale ")
+    
+    }
+    
+    cafeElegido(pregunta)
+    
+    if(pregunta == 1){
+    
+    console.log("tu cafe sale: " + cafe[0].precio);
+    
+    } else {
+    
+    console.log("tu cafe sale: " + cafe[1].precio);
+    
+    }
 
 
 
